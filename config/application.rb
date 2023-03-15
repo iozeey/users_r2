e6bf36f4a375fc8f46fb25f85ce2ffa4d1e1ddc4
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2023-03-15 21:24:16 +0500

    Add sidekiq and configure

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module UsersR2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :sidekiq
   end
 end
