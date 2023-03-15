aa66c24dc471e8904d0833546eb092c6e62a8dee
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2023-03-15 14:00:56 +0500

    Search for name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
         records = User.all
         records = records.where('gender ILIKE ?', "#{params[:gender]}%") if params[:gender].present?
         records = records.where('height ILIKE ?', "#{params[:height]}%")  if params[:height].present?
+        records = records.where('name ILIKE ?', "#{params[:name]}%")  if params[:name].present?
+        records = records.where('email ILIKE ?', "#{params[:email]}%")  if params[:email].present?
         order_by(records, params)
     end
 end
