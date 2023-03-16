30b45d1bfadf34191c868fc51e4a747b758f5f19
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2023-03-16 14:43:17 +0500

    Search case sensitive pg columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
         records = User.all
         records = records.where('gender ILIKE ?', "#{params[:gender]}%") if params[:gender].present?
         records = records.where('height ILIKE ?', "#{params[:height]}%")  if params[:height].present?
-        records = records.where('firstName ILIKE ?', "#{params[:name]}%")  if params[:name].present?
-        records = records.where('lastName ILIKE ?', "#{params[:name]}%")  if params[:name].present?
-        records = records.where('maidenName ILIKE ?', "#{params[:name]}%")  if params[:name].present?
+        records = records.where('"firstName" ILIKE ?', "#{params[:firstName]}%")  if params[:firstName].present?
+        records = records.where('"lastName" ILIKE ?', "#{params[:lastName]}%")  if params[:lastName].present?
+        records = records.where('"maidenName" ILIKE ?', "#{params[:maidenName]}%")  if params[:maidenName].present?
         records = records.where('email ILIKE ?', "#{params[:email]}%")  if params[:email].present?
         order_by(records, params)
     end
