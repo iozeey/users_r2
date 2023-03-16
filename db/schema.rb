39e156bc79f816f40467de503d291b7823e0e342
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2023-03-16 15:02:10 +0500

    Upgrade to rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_15_074921) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_100105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,8 +42,8 @@ ActiveRecord::Schema.define(version: 2023_03_15_074921) do
     t.jsonb "address", default: "{}"
     t.jsonb "bank", default: "{}"
     t.jsonb "company", default: "{}"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
