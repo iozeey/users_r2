61e2860b7010bda7ca2958b79bfc865357004a8d
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2023-03-17 17:18:43 +0500

    Add mailer job enqued or not

--- test/sidekiq/user_mailer_job_test.rb
+++ test/sidekiq/user_mailer_job_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
+require 'minitest/autorun'
+require 'sidekiq/testing'
+
 class UserMailerJobTest < Minitest::Test
-  def test_example
-    skip "add some examples to (or delete) #{__FILE__}"
+  def test_load_user_works
+    assert_equal 0, UserMailerJob.jobs.length
+    job_params = {}
+    job_params["email"] = 'test@test.com'
+    UserMailerJob.perform_in(30.minutes, job_params)
+    assert_equal 1, UserMailerJob.jobs.length
   end
 end
